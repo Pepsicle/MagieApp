c0ea34bb252e782ec0ec273312b7acb792178a5e
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-03-22 11:08:00 +0100

    test

--- MagieApp/Program.cs
+++ MagieApp/Program.cs
@@ -6,7 +6,7 @@ namespace MagieApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wld!");
         }
     }
 }
