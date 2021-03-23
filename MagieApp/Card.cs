4819a38a56d96eea9b355192e72ea8d2ffc1452e
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-03-23 09:43:46 +0100

    Second push
    All classes exist

---
+++ MagieApp/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CardValues = MagieApp.CardValues.Values;
+
+namespace MagieApp
+{
+    class Card
+    {
+        public Card(int value)
+        {
+            CardVal = value;
+            Marked = false;
+            ChosenRow = false;
+        }
+
+        public int CardVal { get; set; }
+        public bool Marked { get; set; }
+        public bool ChosenRow { get; set; }
+
+
+    }
+}
---
+++ MagieApp/CardValues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagieApp
+{
+    public class CardValues
+    {
+        public enum Values
+        {
+            a = 1,
+            b = 2, 
+            c = 3,
+            d = 4,
+            e = 5,
+            f = 6,
+            g = 7,
+            h = 8,
+            i = 9,
+            j = 10,
+            k = 11,
+            l = 12,
+            m = 13,
+            n = 14,
+            o = 15,
+            p = 16
+        }
+    }
+}
