5c003fb7ff30eb27c94a7cb4129acba9831d2629
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-03-26 15:56:02 +0100

    end of day push

--- MagieApp/Column.cs
+++ MagieApp/Column.cs
@@ -6,5 +6,16 @@ namespace MagieApp
 {
     class Column
     {
+        public List<Row> Rows { get; private set; }
+        private int width = 4;
+
+        public Column(int width)
+        {
+            Rows = new List<Row>();
+            for (int i = 0; i < width; i++)
+            {
+                Rows.Add(new Row());
+            }
+        }
     }
 }
--- MagieApp/Game.cs
+++ MagieApp/Game.cs
@@ -6,5 +6,21 @@ namespace MagieApp
 {
     class Game
     {
+        public Game(int width)
+        {
+            Width = width;
+            GridList = new List<Row>();
+            Rows = new List<Row>();
+            for (int i = 0; i < Width; i++)
+            {
+                Rows.Add(new Column(Width));
+            }
+        }
+
+        private int Width { get; set; }
+        private List<Row> GridList { get; set; }
+        private List<Row> Rows { get; set; }
+
+
     }
 }
